2b9e6b04e64e4ea936ba7668b3117f5044f552d7
Katie Soldau <ksoldau@gmail.com>
2020-07-15 19:12:35 +1000

    filter a users climbs

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,5 +1,18 @@
-function climbs(parent, args, context) {
-  return context.prisma.user({ id: parent.id }).climbs();
+async function climbs(parent, args, context) {
+  const where = args.filter
+    ? {
+        OR: [
+          { grade_contains: args.filter },
+          { tickType_contains: args.filter },
+        ],
+      }
+    : {};
+
+  const climbs = await context.prisma.user({ id: parent.id }).climbs({
+    where,
+  });
+
+  return climbs;
 }
 
 module.exports = {
